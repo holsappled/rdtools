0e43e960d96b6f9688087cf575ceb5665cc98e61
derekhols <derekhols31@gmail.com>
2018-11-21 14:14:43 -0700

    Deployed e1c476b with MkDocs version: 1.0.4

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+
+
+Welcome documentation!
+==================================
+
+guide
+^^^^^^
+
+..toctree::
+  :maxdepth: 2
+
+    license
+    helps
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:55,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{aaaaddwswdwadmaxdepth:[],caption:[],content:[],help:0,index:0,licens:0,maxdepth:0,modul:0,page:0,search:0,toctre:0},titles:["Welcome documentation!"],titleterms:{doctut:[],document:0,guid:0,indic:0,iuhu:[],tabl:0,tohi:[],uhouh:[],welcom:0}})
